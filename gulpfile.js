61f5d4cd6c535e79b450b1c8b9f7675df293dd44
rsanlazaro <rsanlazaro@hotmail.com>
2022-02-12 02:47:22 -0600

    Progess on project

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+
+function css(done) {
+
+    src('src/scss/**/*.scss') // Identificar el archivo .SCSS a compilar
+        .pipe( plumber())
+        .pipe( sass() ) // Compilarlo
+        .pipe( dest('build/css') ) // Almacenarla en el disco duro
+
+    done();
+}
+
+function dev(done){
+    watch('src/scss/**/*.scss',css);
+    console.log('LISTO');
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
